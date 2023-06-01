cf7914413505311a051ba395bb8468e2b9b33f0e
Sudhir <Sudhirkaul1@gmail.com>
2023-06-01 23:39:27 +1200

    Cleaned

--- FeatureFlags/Program.cs
+++ FeatureFlags/Program.cs
@@ -40,29 +40,5 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseAzureAppConfiguration();
 
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//};
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateTime.Now.AddDays(index),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast");
-
 app.MapControllers();
 app.Run();
-
-//internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-//{
-//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-//}
